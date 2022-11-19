e0bae1b330e2bd28c7b5684cae06978247fe712c
Matheus Monteiro Schran <matheuschran@hotmail.com>
2022-11-19 10:07:14 -0300

    [ eslint & feature ] instala eslint em ambas aplicações cria rotas iniciais do front cria pagina NotFound cria pagina Login cria pagina Home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Navigate, Route } from 'react-router-dom';
+import Login from './pages/login';
+import NotFound from './pages/notFount';
+import Home from './pages/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={ <Home /> } />
+      <Route path="/login" element={ <Login /> } />
+      <Route path="/404" element={ <NotFound /> } />
+      <Route path="*" element={ <Navigate to="/404" /> } />
+    </Routes>
   );
 }
 
