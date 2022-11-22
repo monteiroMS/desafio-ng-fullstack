c5bd2e7b8f86af52fd1802055db3c6acaac59fea
Matheus Monteiro Schran <matheuschran@hotmail.com>
2022-11-22 19:39:16 -0300

    [style] finaliza estilização do login e da página de registro

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import UserForm from '../../components/userForm';
+import styles from './styles.module.css';
 
 function Login() {
   return (
-    <div>
-      <UserForm type="login" />
-      <p>Ainda não é cadastrado?</p>
-      <Link
-        to="/register"
-      >
-        Criar conta NG.CASH
-      </Link>
-    </div>
+    <main className={ styles.container }>
+      <div className={ styles.loginBox }>
+        <h1>Fazer Login</h1>
+        <UserForm type="login" />
+        <p>Ainda não é cadastrado?</p>
+        <Link
+          to="/register"
+          className={ styles.btn }
+        >
+          Criar conta NG.CASH
+        </Link>
+      </div>
+    </main>
   );
 }
 
--- frontend/src/pages/register/index.tsx
+++ frontend/src/pages/register/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import UserForm from '../../components/userForm';
+import styles from './styles.module.css';
 
 function Register() {
   return (
-    <UserForm type="signin" />
+    <div className={ styles.container }>
+      <div className={ styles.registerBox }>
+        <h1>Cadastro</h1>
+        <UserForm type="signin" />
+      </div>
+    </div>
   );
 }
 
