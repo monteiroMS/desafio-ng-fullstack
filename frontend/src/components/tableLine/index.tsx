3d8bb0569c67806189d426139da3e33309f6cdb8
Matheus Monteiro Schran <matheuschran@hotmail.com>
2022-11-22 17:39:26 -0300

    [style] finaliza estilização da tabela de transações

--- frontend/src/components/tableLine/index.tsx
+++ frontend/src/components/tableLine/index.tsx
@@ -3,20 +3,22 @@ import { UserContext } from '../../context/userProvider';
 import ITransaction from '../../interfaces/ITransactions';
 import formatToReal from '../../utils/formatToReal';
 import getDate from '../../utils/getDate';
+import styles from './styles.module.css';
 
 function TableLine({ transaction }: { transaction: ITransaction }) {
   const { user } = useContext(UserContext);
 
-  // se a conta creditada é a mesma conta do usuario, entao foi um cash-in
   const type = transaction.creditedAccountId === user.accountId
     ? 'debitedFrom'
     : 'creditedIn';
 
   return (
-    <tr>
+    <tr className={ styles.container }>
       <td>{ getDate(transaction.createdAt) }</td>
       <td>{ transaction[type].user.username }</td>
-      <td>
+      <td
+        className={ styles[type === 'debitedFrom' ? 'cash-in' : 'cash-out'] }
+      >
         {
           type === 'debitedFrom'
             ? `+ ${formatToReal(Number(transaction.value))}`
